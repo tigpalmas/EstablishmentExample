40e84400aa8520bd4522814330da9cacbe40aa12
Tiago Palmas <tigpalmas@gmail.com>
2017-11-11 20:31:10 -0200

    Working on ShopFragment

--- app/src/main/java/com/example/tiago/establishmentexample/barFragment/BaseBarFragment.java
+++ app/src/main/java/com/example/tiago/establishmentexample/barFragment/BaseBarFragment.java
@@ -6,11 +6,22 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.tiago.establishmentexample.R;
+import com.example.tiago.establishmentexample.domain.MessagePush;
+import com.example.tiago.establishmentexample.network.EstablishmentProvider;
+import com.example.tiago.establishmentexample.network.EstablishmentService;
+import com.example.tiago.establishmentexample.product.MVP;
+import com.example.tiago.establishmentexample.utils.AppPreferenceTools;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +31,11 @@ public class BaseBarFragment extends Fragment {
     private ViewPager viewPager;
     private ViewPagerCasaDetalheAdapter viewPagerCasaDetalheAdapter;
 
+    private Button btnPush;
+    private AppPreferenceTools mAppPreferenceTools;
+    private EstablishmentService mTService;
+    private MVP.PresenterProduct mPresenter;
+
 
 
     @Override
@@ -27,8 +43,19 @@ public class BaseBarFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View layout =  inflater.inflate(R.layout.fragment_base_bar, container, false);
+        mAppPreferenceTools = new AppPreferenceTools(getContext());
+        EstablishmentProvider provider  = new EstablishmentProvider();
+        mTService = provider.getmService();
         tabLayout = (TabLayout) layout.findViewById(R.id.tabs);
         viewPager = (ViewPager) layout.findViewById(R.id.viewPager);
+        btnPush = (Button) layout.findViewById(R.id.btn_notification);
+
+        btnPush.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
 
         return layout;
     }
@@ -46,9 +73,39 @@ public class BaseBarFragment extends Fragment {
 
         viewPager.setAdapter(viewPagerCasaDetalheAdapter);
         tabLayout.setupWithViewPager(viewPager);
-      tabLayout.getTabAt(0).setIcon(R.drawable.ic_calendar_red);
-      tabLayout.getTabAt(1).setIcon(R.drawable.ic_local_red);
-      tabLayout.getTabAt(2).setIcon(R.drawable.ic_coment_red);
+      tabLayout.getTabAt(0).setIcon(R.drawable.calendar);
+      tabLayout.getTabAt(1).setIcon(R.drawable.map_marker);
+      tabLayout.getTabAt(2).setIcon(R.drawable.message);
 
+    }
+
+
+    private void sendMessage(){
+        EstablishmentProvider provider  = new EstablishmentProvider();
+        mTService = provider.getmService();
+
+        MessagePush push = new MessagePush();
+        push.key = "tag1";
+        push.value = mAppPreferenceTools.getOneSignalId();
+        push.title = "Seu Bar aqui ";
+        push.message = "que tal avisar seus clientes daquela  promoção hoje?";
+        push.largeIcon = "http://maisro.com.br/wp-content/uploads/2015/09/Foto-Marcelo-Gladson.jpg";
+
+        Call<MessagePush> call = mTService.sendPushMessage(push);
+        call.enqueue(new Callback<MessagePush>() {
+            @Override
+            public void onResponse(Call<MessagePush> call, Response<MessagePush> response) {
+                if(response.isSuccess()){
+                    Log.i("teste", "sucesso");
+                }else{
+                    Log.i("teste", "erro");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MessagePush> call, Throwable t) {
+
+            }
+        });
     }
 }
